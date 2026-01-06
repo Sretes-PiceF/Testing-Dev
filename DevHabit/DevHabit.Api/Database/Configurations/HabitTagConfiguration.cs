f997d7be67d7902ce89c49ce30c59c2e230d29e7
Sretes-PiceF <7afajarihsan@gmail.com>
2026-01-06 20:20:41 +0700

    seven commit

--- DevHabit/DevHabit.Api/Database/Configurations/HabitTagConfiguration.cs
+++ DevHabit/DevHabit.Api/Database/Configurations/HabitTagConfiguration.cs
@@ -8,14 +8,14 @@ public sealed class HabitTagConfiguration : IEntityTypeConfiguration<HabitTag>
 {
     public void Configure(EntityTypeBuilder<HabitTag> builder)
     {
-        builder.HasKey(ht => new { ht.HabitId, ht, TagId });
+        builder.HasKey(ht => new { ht.HabitId, ht.TagId });
 
         builder.HasOne<Tag>()
         .WithMany()
         .HasForeignKey(ht => ht.TagId);
 
         builder.HasOne<Habit>()
-        .WithMany(h => h.HabitTags);
+        .WithMany(h => h.HabitTags)
         .HasForeignKey(ht => ht.HabitId);
     }
 }
